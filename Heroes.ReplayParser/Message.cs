985120a6affcb1a4188bd97b104817cfb6cad9a2
Kevin Oliva <kevin.k.oliva@gmail.com>
2016-07-17 11:07:13 -0500

    Updated the parsing of ReplayMessageEvents

---
+++ Heroes.ReplayParser/Message.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using static Heroes.ReplayParser.ReplayMessageEvents;
+
+namespace Heroes.ReplayParser
+{
+    public class Message
+    {
+        public MessageEventType MessageEventType { get; set; }
+        public MessageBase MessageDisplayed { get; set; }
+        public TimeSpan Timestamp { get { return MessageDisplayed.Timestamp; } }
+
+        public override string ToString()
+        {
+            return MessageDisplayed.ToString();
+        }
+    }
+}
